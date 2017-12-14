30a3a4d234537edd27beee5c399fc4d5f99fcf22
dyindude <dyindude@github.dyindude.net>
2017-12-13 21:49:14 -0600

    packer now correctly launches ansible playbook for provisioning Gentoo

--- scripts/ansible.sh
+++ scripts/ansible.sh
@@ -1,1 +1,8 @@
-echo foo
+mkdir -p /tmp/bootstrap/gentoo-ansible
+cd /tmp/bootstrap
+wget https://github.com/pypa/virtualenv/archive/15.1.0.tar.gz -O /tmp/bootstrap/virtualenv.tar.gz
+tar xfvz virtualenv.tar.gz
+python /tmp/bootstrap/virtualenv*/virtualenv.py -p $(which python2) ansible
+source ansible/bin/activate
+pip install --upgrade pip ansible
+echo ansible installed
